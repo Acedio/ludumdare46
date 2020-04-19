69a0c6a5c9b5fdb6a28c31d66f730d57edc39b76
Jason <jsimmons199x@gmail.com>
2020-04-19 13:02:51 -0700

    Added test tile map stuff.

---
+++ LudumDare46/asset_dir/TileMap2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="TileMap2" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="TileMap2.png" trans="ff00ff" width="256" height="256"/>
+</tileset>
